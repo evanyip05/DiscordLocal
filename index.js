426ba9b22986262a57558067c076950eee74dee5
Evan Yip <evanyip05@gmail.com>
2022-08-31 16:15:05 -0700

    added localtunnel and discord client

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
+// imports
 const process = require("process");
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path")
+const localtunnel = require("localtunnel");
+const Discord = require("discord.js");
+const {google} = require("googleapis");
+const path = require("path");
+const fs = require("fs");
+
+// discord client object and express object
+const client = new Discord.Client({intents: ["GUILDS", "GUILDS_MESSAGES"]});
 const app = express();
 
-const {google} = require("googleapis");
-
+// finals
+const SHEETID = "15f3IR2T0ItInwfv9xe1JJmtzTq5W2YVlHSgZKbIq24Q";
+const DTOKEN = JSON.parse(fs.readFileSync("token.json")).token();
 const PORT = 2147;
 
-// sheet id
-const spreadsheetId = "15f3IR2T0ItInwfv9xe1JJmtzTq5W2YVlHSgZKbIq24Q";
+// add public to server
+app.use(express.static("public"));
+app.use(express.json({limit: "1mb"}));
 
+client.login(DTOKEN);
+client.on("ready", () => {console.log("bot ready")});
+
+// write values to gsheet via id, takes value and column
 async function appendValue(spreadsheetId, value, col) {
-
-    const api = getSheetsAPI();
-    const auth = getAuth();
+    const api = getSheetsAPI(); const auth = getAuth();
 
 	await api.spreadsheets.values.append({
         auth, spreadsheetId, range: "Sheet1!"+col+":"+col,
-        valueInputOption: "RAW",
-        resource: {
-        	values: [
-        	    [value,],
-        	],
-        }	
+        valueInputOption: "RAW", resource: {values: [[value,],],}	
     });
 }
 
-async function getClient() {
-	// Create client instance for auth
-    return await getAuth().getClient();
-}
+// Create client instance for auth
+async function getClient() {return await getAuth().getClient();}
 
-function getSheetsAPI() {
-    // Instance of Google Sheets API
-    return google.sheets({ version: "v4", auth: getClient()});
-}
+// Instance of Google Sheets API
+function getSheetsAPI() {return google.sheets({ version: "v4", auth: getClient()});}
 
+// auth
 function getAuth() {
     return new google.auth.GoogleAuth({
         keyFile: "credentials.json",
@@ -44,58 +48,46 @@ function getAuth() {
     });
 }
 
-app.use(express.static("public"));
-app.use(express.json({limit: "1mb"}));
+// root homepage
+app.get("/", (req, res) => {res.sendFile(path.resolve("public", "display.html"))});
 
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve("public", "display.html")+ "");	
+// get file from public
+app.get("/getFile/:file", (req, res) => {
+    console.log(path.resolve("public", req.params.file));
+    res.sendFile(path.resolve("public", req.params.file));
 });
 
-app.post("/addToSheet", (req, res) => {
-    console.log(req.body.inputInfo);
-    appendValue(spreadsheetId, req.body.inputInfo, "A");
-});
-
-
-app.listen(PORT, () => console.log("running on " + PORT));
-
-/*
-// send vals to spreadsheet
-        await appendValues(googleSheets, auth, spreadsheetId, message, "A",);
-async function init() {
-     // auth from args
-    const auth = new google.auth.GoogleAuth({
-        keyFile: args[2],
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-    });
-
-    // Create client instance for auth
-    const client = await auth.getClient();
-
-    // Instance of Google Sheets API
-    const googleSheets = google.sheets({ version: "v4", auth: client });
-}
-
-app.get("/GOTO/:route", (req, res) => {
-    console.log(req.params.route);
+// send discord message via url get req
+app.get("/msgDiscord/:channelID/:message", (req, res) => {
+	client.channels.get(req.params.channel).send(req.params.message);
 	res.redirect("/");
 });
 
-app.get("/OPEN/:html", (req, res) => {
-	res.sendFile(path.resolve("public", req.params.html));
+// append value to gsheet via url get req
+app.get("/addToSheet/:string/:col", (req, res) => {
+    appendValue(SHEETID, req.params.string, req.params.col);
+    res.redirect("/");	
 });
 
-app.get("/SEND/:message", (req, res) => {
-    console.log("sent");
-    res.sendFile(path.resolve("public", "sent.html")+ "");	
+// setup localtunnel
+(async () => {
+	const url = await localtunnel({port: PORT}).url;
+
+	await appendValue(SHEETID, url, "A");
+
+	tunnel.on("close", () => {console.log("tunnelClosed")});
+
+	app.listen(PORT, () => console.log("running on " + PORT + "\nwith url:   " + url));
+})();
+
+// discord bot listen
+client.on("message", async message => {
+    await appendValue(SHEETID, message.channel.name + " ch: ", "A");
+    await appendValue(SHEETID, message.author.tag + ":   " + message.content, "A");
+    await appendValue(SHEETID, "----------------------------------------------------", "A");
 });
 
-// note: {valueName: "value", v2: "val2"} = json obj
-app.get("/GET/:range", (req, res) => {
-	res.status(200).json({info: "serverSideStuff"});
-});
 
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+/*
 
 */
